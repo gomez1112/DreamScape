2e0a5ccf16115db008779461e38d90a60462abf0
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2024-01-27 17:51:31 -0500

    Initial Commit

---
+++ DreamScape/DreamScapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamScapeApp.swift
+//  DreamScape
+//
+//  Created by Gerard Gomez on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct DreamScapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
